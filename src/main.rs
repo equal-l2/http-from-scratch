2737841d2d6bc793dacaf6265051f40e1d752e8e
equal-l2 <eng.equall2@gmail.com>
2022-05-23 13:16:19 +0900

    Added type for header names

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::borrow::Cow;
+use std::collections::HashMap;
+use std::convert::Infallible;
 use std::io::{Read, Write};
 use std::net;
 
@@ -12,10 +14,7 @@ macro_rules! dump_bytestr {
     };
 
     ($v: expr) => {
-        eprintln!(
-            "ANONYMOUS:\n{}\n!!!",
-            std::str::from_utf8($v).expect("valid utf-8 stream")
-        )
+        dump_bytestr!("ANONYMOUS", $v)
     };
 }
 
@@ -26,6 +25,34 @@ enum Method {
     Get,
     Head,
     Post,
+}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+enum HeaderName {
+    Accept,
+    Connection,
+    ContentLength,
+    Host,
+    Referer,
+    UserAgent,
+    Other(String),
+}
+// TODO: parse from &[u8] instead of from &str?
+
+impl std::str::FromStr for HeaderName {
+    type Err = Infallible;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lower = s.to_lowercase();
+        Ok(match lower.as_str() {
+            "accept" => Self::Accept,
+            "connection" => Self::Connection,
+            "content-length" => Self::ContentLength,
+            "host" => Self::Host,
+            "referer" => Self::Referer,
+            "user-agent" => Self::UserAgent,
+            _ => Self::Other(lower),
+        })
+    }
 }
 
 impl TryFrom<&[u8]> for Method {
@@ -48,17 +75,21 @@ struct RequestLine<'a> {
 }
 
 #[derive(Debug)]
-struct RequestLineParseError(String);
+enum RequestLineParseError {
+    MissingMethod,
+    UnsupportedMethod,
+    MissingPath,
+    MissingVersion,
+}
 
 impl std::fmt::Display for RequestLineParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "failed to parse Request-Line: {}", self.0)
-    }
-}
-
-impl From<&str> for RequestLineParseError {
-    fn from(s: &str) -> Self {
-        RequestLineParseError(s.into())
+        match self {
+            Self::MissingMethod => write!(f, "method was not found"),
+            Self::UnsupportedMethod => write!(f, "method was unsupported"),
+            Self::MissingPath => write!(f, "path was not found"),
+            Self::MissingVersion => write!(f, "version was not found"),
+        }
     }
 }
 
@@ -66,13 +97,12 @@ impl<'a> TryFrom<&'a [u8]> for RequestLine<'a> {
     type Error = RequestLineParseError;
     fn try_from(data: &'a [u8]) -> Result<Self, Self::Error> {
         let mut fields = data.split(|&ch| ch == b' ');
-        let method = fields
-            .next()
-            .ok_or("Method not found")?
+        let method_bytes = fields.next().ok_or(Self::Error::MissingMethod)?;
+        let method = method_bytes
             .try_into()
-            .map_err(|_| "Unsupported method")?;
-        let path = fields.next().ok_or("Path not found")?;
-        let version = fields.next().ok_or("Version not found")?;
+            .map_err(|_| Self::Error::UnsupportedMethod)?;
+        let path = fields.next().ok_or(Self::Error::MissingPath)?;
+        let version = fields.next().ok_or(Self::Error::MissingVersion)?;
         Ok(RequestLine {
             method,
             path: String::from_utf8_lossy(path),
@@ -84,7 +114,7 @@ impl<'a> TryFrom<&'a [u8]> for RequestLine<'a> {
 #[derive(Debug)]
 struct RequestHeader<'a> {
     request_line: RequestLine<'a>,
-    headers: std::collections::HashMap<&'a str, &'a str>,
+    headers: HashMap<HeaderName, &'a str>,
 }
 
 fn find_pattern(data: &[u8], pattern: &[u8]) -> Option<usize> {
@@ -129,11 +159,21 @@ enum RequestHeaderParseError {
     Header(HeaderParseError),
 }
 
+impl std::fmt::Display for RequestHeaderParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::RequestLine(e) => write!(f, "failed to parse Request-Line: {}", e),
+            Self::Header(e) => write!(f, "failed to parse Header: {}", e),
+        }
+    }
+}
+
 fn parse_request_header(data: &[u8]) -> Result<RequestHeader, RequestHeaderParseError> {
     // SAFETY: data forms proper header
 
     let request_line_tail = find_newline(data).expect("newline must exist");
-    let request_line = data[..=request_line_tail]
+    let request_line_bytes = &data[..=(request_line_tail - 2)]; // drop CRLF
+    let request_line = request_line_bytes
         .try_into()
         .map_err(RequestHeaderParseError::RequestLine)?;
 
@@ -147,10 +187,11 @@ fn parse_request_header(data: &[u8]) -> Result<RequestHeader, RequestHeaderParse
             Some(local_tail) => {
                 // local_tail is based on data[head..]
                 let tail = head + local_tail;
-                //dump_bytestr!("HEADER", &data[head..=tail]);
-                let (name, value) =
-                    parse_header(&data[head..=tail]).map_err(RequestHeaderParseError::Header)?;
-                headers.insert(name, value);
+                let header_bytes = &data[head..=(tail - 2)]; // drop CRLF
+                let (kind, value) =
+                    parse_header(header_bytes).map_err(RequestHeaderParseError::Header)?;
+                let value = value.trim(); // drop OWS
+                headers.insert(kind, value);
                 head = tail + 1;
             }
             None => unreachable!(),
@@ -165,20 +206,31 @@ fn parse_request_header(data: &[u8]) -> Result<RequestHeader, RequestHeaderParse
 #[derive(Debug)]
 enum HeaderParseError {
     NoSeparator,
-    InvalidUtf8Name(std::str::Utf8Error),
-    InvalidUtf8Value(std::str::Utf8Error),
+    InvalidUtf8Name,
+    InvalidUtf8Value,
 }
 
-fn parse_header(data: &[u8]) -> Result<(&str, &str), HeaderParseError> {
-    // assume header name and value are UTF-8
+impl std::fmt::Display for HeaderParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NoSeparator => write!(f, "separator colon was missing"),
+            Self::InvalidUtf8Name => write!(f, "name was invalid UTF-8",),
+            Self::InvalidUtf8Value => write!(f, "value was invalid UTF-8",),
+        }
+    }
+}
+
+fn parse_header(data: &[u8]) -> Result<(HeaderName, &str), HeaderParseError> {
+    // assume data is proper header, and header name and value are UTF-8
     let split = data.splitn(2, |v| v == &b':').collect::<Vec<_>>();
     debug_assert!(split.len() <= 2);
     match split.as_slice() {
-        [name_byte, value_byte] => {
-            let name = std::str::from_utf8(name_byte).map_err(HeaderParseError::InvalidUtf8Name)?;
-            let value =
-                std::str::from_utf8(value_byte).map_err(HeaderParseError::InvalidUtf8Value)?;
-            Ok((name, value))
+        [name_bytes, value_bytes] => {
+            let name = std::str::from_utf8(name_bytes)
+                .map_err(|_| HeaderParseError::InvalidUtf8Name)?;
+            let value = std::str::from_utf8(value_bytes)
+                .map_err(|_| HeaderParseError::InvalidUtf8Value)?;
+            Ok((name.parse().unwrap(), value))
         }
         _ => Err(HeaderParseError::NoSeparator),
     }
@@ -194,12 +246,11 @@ fn handle_stream(stream: &mut net::TcpStream) -> Result<(), Error> {
                 if let Some(header_tail) = find_header_end(&buf[..=tail]) {
                     match parse_request_header(&buf[..=(header_tail - 2)]) {
                         Ok(header) => {
-                            eprintln!("{:?}", header);
+                            eprintln!("{:#?}", header);
                             // TODO: read body using Content-Length
                         }
                         Err(e) => {
-                            eprintln!("Error: {:?}", e);
-                            stream.shutdown(std::net::Shutdown::Read)?;
+                            eprintln!("Handler error: {}", e);
                             bad_request(stream)?;
                         }
                     }
@@ -207,7 +258,7 @@ fn handle_stream(stream: &mut net::TcpStream) -> Result<(), Error> {
                     break 'main;
                 } else if buf.len() == tail + 1 {
                     // buffer is filled up yet header is not found
-                    eprintln!("Error: buffer is full but header is not found yet");
+                    eprintln!("Handler error: buffer is full but header is not found yet");
                     bad_request(stream)?;
                     return Ok(());
                 }
@@ -221,10 +272,6 @@ fn handle_stream(stream: &mut net::TcpStream) -> Result<(), Error> {
 fn response(stream: &mut net::TcpStream, code: u16, mesg: &str) -> Result<(), Error> {
     write!(stream, "HTTP/1.1 {} {}", code, mesg)?;
     stream.write_all(b"\r\n\r\n")?;
-
-    // TODO: keep-alive
-    stream.shutdown(std::net::Shutdown::Both)?;
-
     Ok(())
 }
 
